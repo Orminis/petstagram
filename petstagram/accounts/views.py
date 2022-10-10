1a1b917e91b7ea7eaf8f5c5604bf9975b97685f2
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-10-10 11:33:15 +0300

    rearrangement of templates.

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -5,21 +5,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def register(request):
+def register_user(request):
     return render(request, template_name="accounts/register-page.html")
 
 
-def login(request):
+def login_user(request):
     return render(request, template_name="accounts/login-page.html")
 
 
-def show_profile_details(request):
+def show_profile(request, pk: int):
     return render(request, template_name="accounts/profile-details-page.html")
 
 
-def edit_profile(request):
+def edit_profile(request, pk: int):
     return render(request, template_name="accounts/profile-edit-page.html")
 
 
-def delete_profile(request):
+def delete_profile(request, pk: int):
     return render(request, template_name="accounts/profile-delete-page.html")
