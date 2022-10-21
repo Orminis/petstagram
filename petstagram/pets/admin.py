7e728cde5759170a1894c7a86fca707f8225d552
Orminis <k.nedialkov@gmail.com>
2022-10-22 01:11:02 +0300

    created Photo model

--- petstagram/pets/admin.py
+++ petstagram/pets/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 from petstagram.pets.models import Pet
+from petstagram.photos.models import Photo
 
 
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
     pass
