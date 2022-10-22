b82b83763453fb58579e4df0f7b015d1d770a359
Orminis <k.nedialkov@gmail.com>
2022-10-22 23:09:39 +0300

    arrangement of Photo model and adding photo to admin panel

--- petstagram/photos/admin.py
+++ petstagram/photos/admin.py
@@ -6,4 +6,17 @@ from petstagram.photos.models import Photo
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('pk',
+                    'description',
+                    'publication_date',
+                    'pets',
+                    )
+
+    @staticmethod
+    def pets(photo_obj):
+        # PhotoAdmin inherits Photo, so it has tagged_pets too
+        # pets returns queryset of pet/s tagged to the picture
+        tagged_pets = photo_obj.tagged_pets.all()
+        if tagged_pets:
+            return ', '.join(obj.name for obj in tagged_pets)
+        return "No Pets!"
--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -1,19 +1,18 @@
-from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from petstagram.basic.model_mixins import StrFromFieldsMixin
+from petstagram.basic.validators import validate_image_less_than_5mgb
 from petstagram.pets.models import Pet
 
 
-def validate_image_less_than_5mgb(image):
-    filesize = image.file.size
-    megabyte_limit = 5.0
-    if filesize > megabyte_limit * (1024**2):
-        raise ValidationError(f"Max file size is 5MBs")
+# Create your models here.
 
 
-class Photo(models.Model):
+class Photo(StrFromFieldsMixin, models.Model):
+    str_fields = ('photo', 'location')
+
+    MEDIA_FILES = 'mediafiles/pet_photo/'
     MIN_DESCRIPTION_LENGTH = 10
     MAX_DESCRIPTION_LENGTH = 300
 
@@ -21,7 +20,7 @@ class Photo(models.Model):
 
     # Requires mediafiles to work correctly
     photo = models.ImageField(
-        upload_to='mediafiles/pet_photo/',
+        upload_to=MEDIA_FILES,
         validators=(validate_image_less_than_5mgb,),
         null=False,
         blank=True,
